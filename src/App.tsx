92847787dba5bc1370527c26e2617dbdba6725e7
NidaArshad1908 <nidaarshad113@gmail.com>
2025-06-05 20:13:04 +0500

    App add

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-import NotFound from "./pages/not-found";   
+import NotFound from "./pages/not-found";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
