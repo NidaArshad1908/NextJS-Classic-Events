6e8b0f38d79b3fc8abf295c2971b8982aa40db77
NidaArshad1908 <nidaarshad113@gmail.com>
2025-10-04 16:03:50 +0500

    Dev-dep issue

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  version: 2,
-  builds: [
-    {
-      src: "package.json",
-      use: "@vercel/next",
-    },
-  ],
+  // version: 2,
+  // builds: [
+  //   {
+  //     src: "package.json",
+  //     use: "@vercel/next",
+  //   },
+  // ],
+  experimental: {
+    turbo: {},
+  },
 };
 
 export default nextConfig;
